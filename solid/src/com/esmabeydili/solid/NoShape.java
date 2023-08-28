a55724d3da7554b05e567e6a0d0ad0abb63d0538
Esma Beydili <106523107+Esma222@users.noreply.github.com>
2023-08-28 17:16:18 +0300

    Update NoShape.java

--- solid/src/com/esmabeydili/solid/NoShape.java
+++ solid/src/com/esmabeydili/solid/NoShape.java
@@ -1,6 +1,6 @@
 package com.esmabeydili.solid;
 
-public class NoShape implements Shape{// Liskov Subtitution -S
+public class NoShape implements Shape{// Liskov Subtitution -L
     @Override
     public double area() {//broke the Liskov substitution rule
         throw new IllegalStateException(" Cannot calculate area");
